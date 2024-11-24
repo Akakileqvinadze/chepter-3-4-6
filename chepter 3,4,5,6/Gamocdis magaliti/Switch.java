69858cb80ef35829bec72778431eeaf7ae3b111b
Akakileqvinadze <akakileqvinadze@yahoo.com>
2024-11-24 16:16:32 +0400

    Add files via upload

---
+++ chepter 3,4,5,6/Gamocdis magaliti/Switch.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int statusi = 1;
+        switch (statusi) {
+            case 1:
+                System.out.println("Name");
+                break;
+            case 2:
+                System.out.println("SurName");
+                break;
+            default:
+                System.out.println("Nan");
+                break;
+        }
+
+    }
+}

126c58a5e61051906ea3a074ba45e813cb62fecc
Akakileqvinadze <akakileqvinadze@yahoo.com>
2025-02-13 21:02:28 +0400

    Update magaliti.java

--- chepter 3,4,5,6/magaliti.java
+++ chepter 3,4,5,6/magaliti.java
@@ -1,1 +1,17 @@
+//  4. დაწერეთ რეკურსიული ფუნქცია სტრიქონის შესაბრუნებლად.
+
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args)  {
+        method(originalStr.length());
+    }
+    static String originalStr = "ABCDEFJHIJ";
+    public static void method(int n) {
+        System.out.println(originalStr.charAt(n-1));
+        method(n-1);
+    }
+}
+
 

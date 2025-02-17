131de7e34a8123b55f82719c57331973f7026e49
Akakileqvinadze <akakileqvinadze@yahoo.com>
2025-02-17 16:13:19 +0400

    Update magaliti2.java

--- chepter 3,4,5,6/magaliti2.java
+++ chepter 3,4,5,6/magaliti2.java
@@ -1,1 +1,23 @@
+// 1) შექმენით შემდგომი ლამბდა გამოსახულებები:Predicate (ამოწმებს არის თუ არა რიცხვი ლუწი)
 
+
+import java.util.function.Predicate;
+
+
+public class Main {
+    public static void main(String[] args) {
+      
+
+        Predicate<Integer>numbers = n ->{
+            if(n%2==0){
+                System.out.println("ლუწია");
+            }
+            else {
+                System.out.println("კენტია");
+            }
+
+            return false;
+        };
+        numbers.test(4);
+    }
+}

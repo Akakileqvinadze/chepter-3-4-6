af124c7cdc0093c3911d6e5e14591590a593c5d9
Akakileqvinadze <akakileqvinadze@yahoo.com>
2025-02-17 16:53:31 +0400

    Update magaliti2.java

--- chepter 3,4,5,6/magaliti2.java
+++ chepter 3,4,5,6/magaliti2.java
@@ -21,3 +21,57 @@ public class Main {
         numbers.test(4);
     }
 }
+
+
+//Consumer (სტრიქონის დაბეჭდვა დიდი ასოებით)
+
+
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Consumer<String> numbers = n -> System.out.println(n.toUpperCase());
+        numbers.accept("akaki");
+    }
+}
+
+//Function (აბრუნებს მოცემული სტრიქონის სიგრძეს)
+
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Function<String,Integer> numbers = n -> n.length();
+        System.out.println(numbers.apply("akaki"));
+
+    }
+}
+
+//Functional Interface(შეასრულებს არითმეტიკული ოპერაციას)
+
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+
+public class Main {
+    public static void main(String[] args) {
+// Functional Interface
+//(შეასრულებს არითმეტიკული ოპერაციას)
+        methodoperation<Integer>number = (a, b) -> a+b;
+        System.out.println(number.apply(5,5));
+    }
+}
+@FunctionalInterface
+interface methodoperation<T>{
+    T apply(T a ,T b);
+}
